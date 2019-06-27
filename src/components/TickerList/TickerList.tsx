3522002c1e6493e26385cb25eacb8b4b6b511bb3
Evgenii Sidorov <evgenii.sidorov4@gmail.com>
2019-06-27 16:10:00 +0300

    :construction: Added search input.

--- src/components/TickerList/TickerList.tsx
+++ src/components/TickerList/TickerList.tsx
@@ -6,13 +6,34 @@ export interface TickerListProps {
     fetchTickers(url: string): void;
 }
 
-export class TickerList extends React.Component<TickerListProps> {
+interface TickerListState {
+    tickerSearchValue: string;
+}
+
+export class TickerList extends React.Component<TickerListProps, TickerListState> {
+    constructor(props: TickerListProps) {
+        super(props);
+        this.state = { tickerSearchValue: "" }
+    }
+
     public componentDidMount() {
         this.props.fetchTickers && this.props.fetchTickers(Urls.getALlTickers());
     }
+
     public render(): JSX.Element {
         return <div>
             <h2>Tickers</h2>
+            <div className="input-group mb-3">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter ticker symbol"
+                    onChange={(event) => this.setState({tickerSearchValue: event.target.value})}
+                />
+                <div className="input-group-append">
+                    <button className="btn btn-outline-secondary" type="button">Search</button>
+                </div>
+            </div>
             <ul>
                 {this.props.tickers && this.props.tickers.slice(0, 10).map((x, index) => {
                     return <li key={`ticker-${index}`}>{x.symbol}</li>
