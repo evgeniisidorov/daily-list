3f3cc9df9d2346f342c714105af90c0fa38fb74a
Evgenii Sidorov <evgenii.sidorov4@gmail.com>
2019-06-30 14:56:02 +0300

    :recycle: Rewrote StarButton component to Functional one.

--- src/components/StarButton/StarButton.tsx
+++ src/components/StarButton/StarButton.tsx
@@ -3,6 +3,11 @@ import React from 'react';
 import { ReactComponent as StarIcon } from './star.svg';
 import { ReactComponent as OutlinedStarIcon } from './outlinestar.svg';
 
-export function StarButton(hasOutline: boolean, onClick: () => void): JSX.Element {
-    return <div className="star-button" onClick={onClick}> {hasOutline ? <OutlinedStarIcon /> : <StarIcon />}</div>;
+interface StarButtonProps {
+    hasOutline: boolean;
+    onClick: () => void;
+}
+export const StarButton: React.FC<StarButtonProps> = (props) => {
+    return <div className="star-button" onClick={props.onClick}> {props.hasOutline ? <OutlinedStarIcon /> : <StarIcon />}</div>;
 };
+
